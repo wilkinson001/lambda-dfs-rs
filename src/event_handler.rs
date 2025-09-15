6d04fe61e99df5390ed95880b156926d84c744eb
Oliver Wilkinson <o.wilko@live.co.uk>
2025-09-15 21:56:52 +0100

    initial commit

---
+++ src/event_handler.rs
@@ -0,0 +1,52 @@
+use aws_lambda_events::event::sqs::SqsEvent;
+use aws_lambda_events::sqs::SqsMessage;
+use iceberg::table::Table;
+use iceberg_catalog_rest::{RestCatalog, RestCatalogConfig};
+use lambda_runtime::{tracing, Error, LambdaEvent};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::env;
+use std::primitive::tuple;
+
+pub(crate) async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+    // Extract some useful information from the request
+    let payload = event.payload;
+    tracing::info!("Payload: {:?}", payload);
+    let catalog = load_catalog();
+    let mut event_map: HashMap<tuple<String, String>, Vec<&SqsMessage>> = HashMap::new();
+
+    for event in payload.records.iter() {
+        let key = get_namespace_and_table(&event);
+        event_map.entry(key).or_insert(vec![]).push(event)
+    }
+
+    Ok(())
+}
+
+fn load_catalog() -> RestCatalog {
+    // Create the REST iceberg catalog.
+    let warehouse = env::var("WAREHOUSE").unwrap();
+    let uri = env::var("URI").unwrap();
+    let catalog_config = RestCatalogConfig::builder()
+        .warehouse(warehouse)
+        .uri(uri)
+        .props(HashMap::new())
+        .build();
+    RestCatalog::new(catalog_config)
+}
+
+fn get_namespace_and_table(message: &SqsMessage) -> tuple<String, String> {
+    let body: String = message.body.to_owned().unwrap();
+    let parsed_body: Value = serde_json::from_str(&body[..]).unwrap();
+    let detail_type = parsed_body["detail-type"].as_str().unwrap();
+    let parts: Vec<&str> = String::from(detail_type).split(".").collect();
+    return (String::from("dfs_") + parts[0], parts[1]);
+}
+
+// 1. Load Catalog
+// 2. Iterate events
+//      a. Calculate Namespace and table
+//      b. Pull S3 data
+//      c. Add to namespace+table list
+// 3. Iterate namespace+table
+//      a. Append data to table
